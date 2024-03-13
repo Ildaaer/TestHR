0f62a5f26f1258c9595bcf38978b8c8bb89e09ed
Danil Ildeikin <rubl.ild7@yandex.ru>
2024-03-13 15:11:24 +0400

    empty project

---
+++ src/main/java/com/example/testhr/TestHrApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testhr/TestHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
